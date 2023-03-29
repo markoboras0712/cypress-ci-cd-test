38c98e671a731637dbd6a3bea2a7e1fc0ef43bd8
markoboras0712 <marko.boras@prototyp.digital>
2023-03-29 15:29:00 +0200

    update yml and add project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:8888',
+    baseUrl: "http://localhost:8888",
   },
-})
+  projectId: "k46iwf",
+});
