26acd102ecdedcf4b44ba45806a27b66d7c1a603
markoboras0712 <marko.boras@prototyp.digital>
2023-03-31 11:34:00 +0200

    update kill processs script

--- .github/scripts/kill-processes.sh
+++ .github/scripts/kill-processes.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-
 # Check if the port number is provided
-if [[ -z "$1" ]]; then
+if [[ -z "$PORT_NAME" ]]; then
+    # If no port number is provided, print an error message and exit
     echo "Please provide the port number to kill"
     exit 1
 fi
 
 # Find the process running on the port and kill it
-echo "Killing process running on port $1"
-if lsof -Pi :$1 -sTCP:LISTEN -t >/dev/null ; then
-    lsof -ti:$1 | xargs kill -9
-    echo "Processes on port $1 have been killed"
+echo "Killing process running on port $PORT_NAME"
+if lsof -Pi :$PORT_NAME -sTCP:LISTEN -t >/dev/null ; then
+    # If there is a process running on the specified port, kill it
+    lsof -ti:$PORT_NAME | xargs kill -9
+    echo "Processes on port $PORT_NAME have been killed"
 else
-    echo "No processes found running on port $1"
+    # If there is no process running on the specified port, print a message
+    echo "No processes found running on port $PORT_NAME"
 fi
