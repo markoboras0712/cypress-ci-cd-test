67ae8a7f355962136935764a85084f65107b4695
markoboras0712 <marko.boras@prototyp.digital>
2023-03-31 10:23:25 +0200

    create cypress env json

--- .github/scripts/print.sh
+++ .github/scripts/print.sh
@@ -2,3 +2,12 @@
 
 cd $1
 ls -la
+
+if [ ! -f "cypress.env.json" ]; then
+  echo "Creating cypress.env.json file"
+  echo '{}' > cypress.env.json
+else
+  echo "Cypress.env.json already exists"
+fi
+
+cat cypress.env.json
