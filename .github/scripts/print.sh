696604f212d1ba1c1f5a6cf103f5a8f70dd93b25
markoboras0712 <marko.boras@prototyp.digital>
2023-03-31 10:24:08 +0200

    update print script

--- .github/scripts/print.sh
+++ .github/scripts/print.sh
@@ -5,7 +5,20 @@ ls -la
 
 if [ ! -f "cypress.env.json" ]; then
   echo "Creating cypress.env.json file"
-  echo '{}' > cypress.env.json
+   echo '{
+    "userFree": {
+      "email": "cypress+ci+'$(openssl rand -hex 4)'@gmail.com",
+      "password": "123456"
+    },
+    "userPaid": {
+      "email": "cypress+48@test.test",
+      "password": "suncica"
+    },
+    "userRelease": {
+      "email": "cypress+40@test.test",
+      "password": "suncica"
+    }
+  }' > cypress.env.json
 else
   echo "Cypress.env.json already exists"
 fi
